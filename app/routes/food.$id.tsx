1534b067db8a41994f0a3c551b9b9c61ce066022
anantariskys <gamersindo555@gmail.com>
2024-12-05 16:38:26 +0700

    Create food.$id.tsx

---
+++ app/routes/food.$id.tsx
@@ -0,0 +1,21 @@
+import { LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import React from "react";
+import PageLayout from "~/layouts/PageLayout";
+
+export const loader: LoaderFunction = async ({ params, request }) => {
+  return params;
+};
+const food = () => {
+  const data = useLoaderData<{ id: number }>();
+  console.log(data);
+  return (
+    <PageLayout>
+      <main className="container  h-screen bg-slate-500">
+        <div>ini halaman {data.id}</div>
+      </main>
+    </PageLayout>
+  );
+};
+
+export default food;
