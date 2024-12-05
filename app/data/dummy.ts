a89c754a2c9f5f9bfd524691ebbc129412d57519
anantariskys <gamersindo555@gmail.com>
2024-12-05 16:38:20 +0700

    Create dummy.ts

---
+++ app/data/dummy.ts
@@ -0,0 +1,28 @@
+import Makanan from "../assets/foodImage.png";
+
+export const dummy = [
+    {
+        id: 1,
+        name: "Soto Ayam",
+        image: Makanan,
+        description: "Perpaduan bumbu seperti kunyit, serai, daun jeruk, lengkuas, dan cabai yang menghasilkan perpaduan rasa gurih dan pedas yang khas."
+    },
+    {
+        id: 2,
+        name: "Rendang",
+        image: Makanan,
+        description: "Masakan daging sapi yang dimasak dengan santan dan rempah-rempah khas seperti cabai, bawang putih, serai, lengkuas, dan kunyit, memberikan cita rasa pedas dan gurih yang kaya."
+    },
+    {
+        id: 3,
+        name: "Nasi Goreng",
+        image: Makanan,
+        description: "Nasi goreng khas Indonesia dengan campuran bumbu bawang putih, bawang merah, dan kecap manis, disajikan dengan telur, ayam, dan sayuran."
+    },
+    {
+        id: 4,
+        name: "Gado-Gado ",
+        image: Makanan,
+        description: "Salad sayuran yang disiram dengan bumbu kacang yang gurih, lengkap dengan lontong, telur rebus, dan kerupuk sebagai pelengkap."
+    }
+];
