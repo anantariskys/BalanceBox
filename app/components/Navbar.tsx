82cf73bef7d1ebad098c67719b1ed08e4e659354
anantariskys <gamersindo555@gmail.com>
2024-12-06 22:38:38 +0700

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 import { Link } from "@remix-run/react";
 const Navbar = () => {
   return (
-    <nav className="w-full py-5 ">
+    <nav className="w-full py-5 fixed top-0 left-0 z-50 bg-white">
       <main className="container flex justify-between items-center ">
         <Link to={'/'}>
 
@@ -12,12 +12,12 @@ const Navbar = () => {
         </Link>
 
         <div className="flex gap-4">
-          <Link to={'/register'}>
+          <Link to={'/auth/register'}>
           <Button type="button" variant="primary-outline" width="w-fit">
             Daftar
           </Button>
           </Link>
-          <Link to={'/login'}>
+          <Link to={'/auth/login'}>
           <Button type="button" variant="default" width="w-fit">
             Masuk
           </Button>
